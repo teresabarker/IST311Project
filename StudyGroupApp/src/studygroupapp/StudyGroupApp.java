0c543b521024cffe07f7bfcd20b9da626a1bc72d
Arawup <>
2019-06-22 18:15:07 -0400

    Created java swing app
    Created the Swing Java application, defined the auto start up, began work on login and verification

---
+++ StudyGroupApp/src/studygroupapp/StudyGroupApp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studygroupapp;
+import javax.swing.*;
+import View.LoginScreen;
+/**
+ *
+ * @author angel
+ */
+public class StudyGroupApp extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable(){
+        public void run(){
+            new LoginScreen().setVisible(true);
+        }
+        });
+    }
+    
+}
