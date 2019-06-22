02cd9b2369e3507471b2b9457ff9f82851d1dfde
Arawup <>
2019-06-22 18:28:53 -0400

    began works on the model for user and study group
    defined some basic fields for user and study group, havent implemented some fields such as calendar due to complexity. No methods stated

---
+++ StudyGroupApp/src/Model/StudyGroup.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author angel
+ */
+public class StudyGroup {
+    String courseName;
+    String courseSection;
+    String instructorName;//TODO maybe implement it as User
+    String location;
+    String time;
+    
+}
--- StudyGroupApp/src/Model/User.java
+++ StudyGroupApp/src/Model/User.java
@@ -5,10 +5,35 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author angel
  */
 public class User {
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    ArrayList<String> userGroups;//TODO change to events type
+    int userType;//change to enum Student, professor, tutor
+    String userDegree;
+    //Calendar calendar;//TODO verify if it's too complex of a feature
+    
+    
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    
     
 }
