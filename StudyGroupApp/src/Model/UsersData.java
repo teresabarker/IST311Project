067208c758890a388bcb0538279e26d35db647e0
Arawup <>
2019-06-22 19:22:17 -0400

    forgot something to save

--- StudyGroupApp/src/Model/UsersData.java
+++ StudyGroupApp/src/Model/UsersData.java
@@ -6,8 +6,11 @@
 package Model;
 import Model.User;
 import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -38,6 +41,33 @@ public class UsersData {
                 }
             }
             decoder.close();
+        } catch (Exception xx)
+        {
+            xx.printStackTrace();
+        }
+    }
+    
+    public void writeUsersData(){
+        User user2 = new User();
+        try
+        {
+            User user;
+            XMLEncoder encoder;
+            encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("UsersData.xml")));
+            user = new User();
+            while (user != null)
+            {
+                try
+                {
+                    user = (User) encoder.writeObject( new User());
+                    usersData.add(user);
+
+                } catch (ArrayIndexOutOfBoundsException theend)
+                {
+                    break;
+                }
+            }
+            encoder.close();
         } catch (Exception xx)
         {
             xx.printStackTrace();
